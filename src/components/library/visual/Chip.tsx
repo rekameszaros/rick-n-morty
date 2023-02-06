fdb53c801dc3585f8f92798cb10dca828e5284a1
Mathias Nielsen <97943929+cph-mtnl@users.noreply.github.com>
2023-02-06 14:56:20 +0100

    added a story

--- src/components/library/visual/Chip.tsx
+++ src/components/library/visual/Chip.tsx
@@ -5,15 +5,29 @@ import { X } from "lucide-react";
 export interface ChipProps {
   text: string;
   onClick: (text: string) => void;
+  variant: "filled" | "outlined";
 }
 
-export default function Chip({ text, onClick }: ChipProps) {
+export default function Chip({ text, onClick, variant = "filled" }: ChipProps) {
   const handleClick = () => {
     onClick(text);
   };
 
+  const getBackgroundColor = () => {
+    if (variant === "filled") {
+      return "#e1f8dc";
+    } else {
+      return "#FFFFFF";
+    }
+  };
+
   return (
-    <div className={styles.chip}>
+    <div
+      className={styles.chip}
+      style={{
+        backgroundColor: getBackgroundColor(),
+      }}
+    >
       <span>{text}</span>
       <button onClick={handleClick}>
         <X color="#00b0c8" size="19" />
