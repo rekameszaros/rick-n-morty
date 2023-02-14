282c4789582ba9bac685242137f4797b0fc9442b
Mathias Nielsen <97943929+cph-mtnl@users.noreply.github.com>
2023-02-14 10:33:10 +0100

    reverted changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,32 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
-import Chip from "./components/library/visual/Chip";
-import ChipList from "./components/library/visual/ChipList";
 
 function App() {
-  const [items, setItems] = useState(["Human", "Dead", "Alive", "Genderless"]);
-
-  const handleRemove = (text: string) => {
-    setItems((previousItems) => {
-      return previousItems.filter((item) => item !== text);
-    });
-  };
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <div
-        style={{
-          width: "300px",
-        }}
-      >
-        <ChipList items={items} onRemoveItem={handleRemove} />
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </div>
   );
 }
