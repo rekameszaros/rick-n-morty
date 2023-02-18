3c542178cc7cecc1294fa220de475f1d8a7b2a20
rekameszaros <rikkemeszaros@gmail.com>
2023-02-18 18:02:42 +0100

    Fetching the data. But /* */  some of the previous works.

--- src/components/library/visual/Card.tsx
+++ src/components/library/visual/Card.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './CardStyle.css';
+import { useCharacters } from "./useCharacters";
 
+/*
 interface CardProps {
   image?: string;
   title: string;
@@ -23,3 +25,26 @@ export default function Card({
     </div>
   );
 };
+*/
+
+export default function Card() {
+
+//now the hook is reusable
+const {error, loading, data} = useCharacters();
+
+  if (loading) return <div>spinner...</div>;
+  if (error) return <div>Something went wrong</div>;
+
+  return (
+    <div className="Card">
+      {data.characters.results.map((character) => {
+        return (
+          <div>
+            <img scr={character.image} />
+            <h2>{character.name}</h2>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { ApolloClient, ApolloProvider,InMemoryCache} from '@apollo/client';
+import { BrowserRouter } from 'react-router-dom';
 
+/*
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+*/
+
+const client = new ApolloClient({
+  uri: "https://rickandmortyapi.com/graphql",
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <ApolloProvider client={client}>
+    <App />
+    </ApolloProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
