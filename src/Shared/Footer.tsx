0023c326e7b7932d5380f8121e9b3dc32fb6f3a3
rekameszaros <rikkemeszaros@gmail.com>
2023-02-28 09:04:58 +0100

    footer

---
+++ src/Shared/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>Mathias Nielsen @ Cph Business Lyngby - Web Development</p>
+    </footer>
+  );
+}
+
+export default Footer;
